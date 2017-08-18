84299a5fc0a8ec836a599da8b19e8d241d38ac4e
piggsoft <piggsoft@163.com>
2017-08-18 23:29:49 +0800

    添加配置

--- operator-provider/src/main/java/com/piggsoft/auto/operator/OperatorProviderApplication.java
+++ operator-provider/src/main/java/com/piggsoft/auto/operator/OperatorProviderApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * Created by piggs on 2017/8/17.
+ * Created by piggs on 2017/8/18.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class OperatorsApplication {
+public class OperatorProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OperatorsApplication.class, args);
+        SpringApplication.run(OperatorProviderApplication.class, args);
     }
-
 }
--- operator-provider/src/main/java/com/piggsoft/auto/operator/api/ApiController.java
+++ operator-provider/src/main/java/com/piggsoft/auto/operator/api/ApiController.java
@@ -1,6 +1,5 @@
 package com.piggsoft.auto.operator.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
